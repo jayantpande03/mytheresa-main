7c58963ce973dab644ef12477bd11951bc7f2e73
jayantpande03 <106484910+jayantpande03@users.noreply.github.com>
2022-05-30 01:22:31 +0530

    update postIntall

--- postInstall.sh
+++ postInstall.sh
@@ -1,4 +1,3 @@
-docker exec -it php74-container bash
 symfony console doctrine:database:create --env=test
 symfony console doctrine:migrations:migrate -n --env=test
 ./bin/phpunit tests/testProducts.php 
